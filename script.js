328b9708b9397d9ed46c74dd26dbad7d20daf6f5
Vanshika Omer <123651941+VanshikaOmer@users.noreply.github.com>
2024-07-14 17:35:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+
+const form = document.getElementById("form");
+const input = document.getElementById("input");
+const todosUl = document.getElementById("todos");
+
+const todos = JSON.parse(localStorage.getItem("todos"));
+
+if (todos) {
+  todos.forEach((todo) => addTodo(todo));
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  addTodo();
+});
+
+function addTodo(todo) {
+  let todoText = input.value;
+
+  if (todo) {
+    todoText = todo.text;
+  }
+
+  if (todoText) {
+    const todoEl = document.createElement("li");
+    if (todo && todo.completed) {
+      todoEl.classList.add("completed");
+    }
+
+    todoEl.innerText = todoText;
+
+    todoEl.addEventListener("click", () => {
+      todoEl.classList.toggle("completed");
+      UpdateLS();
+    });
+
+    todoEl.addEventListener("contextmenu", (e) => {
+      e.preventDefault();
+
+      todoEl.remove();
+      UpdateLS();
+    });
+
+    todosUl.appendChild(todoEl);
+
+    input.value = "";
+
+    UpdateLS();
+  }
+}
+
+function UpdateLS() {
+  todoEl = document.querySelectorAll("li");
+
+  const todos = [];
+
+  todoEl.forEach((todoEl) => {
+    todos.push({
+      text: todoEl.innerText,
+      completed: todoEl.classList.contains("completed"),
+    });
+  });
+
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
